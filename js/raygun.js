18a421c188e875cadb8fd753bdcc47268f32261d
markhuge <mark@segfawlt.net>
2015-05-11 18:50:06 -0700

    gh-pages update

---
+++ js/raygun.js
@@ -0,0 +1,12 @@
+var $j = jQuery.noConflict();
+			 
+			 $j(document).ready(function(){	
+			$j("#slider").easySlider({
+
+			});
+			$j("#slider2").easySlider({
+	
+				prevId: 'prevBtn2',
+				nextId: 'nextBtn2'	
+			});			
+		});	
